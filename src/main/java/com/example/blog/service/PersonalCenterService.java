47184e779ee2d6e9d7d8e23462f032a5d3c7404b
keeep <690486631@qq.com>
2019-08-22 15:31:01 +0800

    vip

--- src/main/java/com/example/blog/service/PersonalCenterService.java
+++ src/main/java/com/example/blog/service/PersonalCenterService.java
@@ -1,8 +1,31 @@
 package com.example.blog.service;
 
+import com.example.blog.entity.Article;
+
+import java.util.List;
+
 /**
  * @Author mayn
  * @Date 2019/8/22 15:20
  */
 public interface PersonalCenterService {
+    /**
+     * 查询该用户总共发表文章数量
+     */
+    Integer selectArticleNumByAccount();
+
+    /**
+     * 查询该用户总共评论量
+     */
+    Integer selectMessageNumByAccount();
+
+    /**
+     * 查询该用户为他人点赞次数
+     */
+    Integer selectArticleStarNumByAccount();
+
+    /**
+     * 最近发布的文章 3篇
+     */
+    List<Article> selectSoonArticle();
 }
--- src/main/java/com/example/blog/service/serverImpl/PersonalCenterServiceImpl.java
+++ src/main/java/com/example/blog/service/serverImpl/PersonalCenterServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.blog.service.serverImpl;
 
+import com.example.blog.entity.Article;
 import com.example.blog.service.PersonalCenterService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @Author mayn
@@ -9,10 +12,23 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class PersonalCenterServiceImpl implements PersonalCenterService {
+    @Override
+    public Integer selectArticleNumByAccount() {
+        return null;
+    }
 
+    @Override
+    public Integer selectMessageNumByAccount() {
+        return null;
+    }
 
+    @Override
+    public Integer selectArticleStarNumByAccount() {
+        return null;
+    }
 
-
-
-
+    @Override
+    public List<Article> selectSoonArticle() {
+        return null;
+    }
 }
